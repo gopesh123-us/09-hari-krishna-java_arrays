e11e195d370fc1cf1dd30c0524cf3d0669d7df69
Gopesh Sharma <gopesh.sharma@gmail.com>
2026-04-23 12:33:51 -0400

    Stop tracking build files (target/ and .class)

---
+++ src/main/java/live/learnjava/java_arrays/TestApp.java
@@ -0,0 +1,12 @@
+package live.learnjava.java_arrays;
+
+/**
+ * Hello world!
+ *
+ */
+public class TestApp 
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World of arrays");
+	}
+}
